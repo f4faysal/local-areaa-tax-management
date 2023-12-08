c978492ad7d2a1d83a0c912ce6a57a1b399f9da8
f4faysal <f4faysals@gmail.com>
2023-12-08 23:01:39 +0600

    Refactor layout and sidebar components

--- src/app/(withlayout)/layout.tsx
+++ src/app/(withlayout)/layout.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import Contents from "@/components/ui/Contents";
 import SideBar from "@/components/ui/Sidebar";
 import { Layout } from "antd";
+import { useState } from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const [collapsed, setCollapsed] = useState(false);
   return (
     <Layout hasSider>
-      <SideBar />
-      <Contents>{children}</Contents>
+      <SideBar collapsed={collapsed} />
+      <Contents collapsed={collapsed} setCollapsed={setCollapsed}>
+        {children}
+      </Contents>
     </Layout>
   );
 };
--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,32 +1,63 @@
 "use client";
-import { Layout } from "antd";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Button, Layout, theme } from "antd";
+import { Header } from "antd/es/layout/layout";
 import UMBreadCrumb from "./UMBreadCrumb";
 
 const { Content } = Layout;
 
-const Contents = ({ children }: { children: React.ReactNode }) => {
+interface ContentsProps {
+  collapsed: boolean;
+  setCollapsed: (collapsed: boolean) => void;
+  children: React.ReactNode;
+}
+
+const Contents: React.FC<ContentsProps> = ({
+  children,
+  setCollapsed,
+  collapsed,
+}) => {
   const base = "admin";
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <Content
-      style={{
-        minHeight: "100vh",
-        color: "black",
-      }}
-    >
-      <UMBreadCrumb
-        items={[
-          {
-            label: `${base}`,
-            link: `/${base}`,
-          },
-          {
-            label: "student",
-            link: `/${base}/student`,
-          },
-        ]}
-      />
-      {children}
-    </Content>
+    <>
+      <Header style={{ padding: 0, background: colorBgContainer }}>
+        <Button
+          type="text"
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={() => setCollapsed(!collapsed)}
+          style={{
+            fontSize: "16px",
+            width: 64,
+            height: 64,
+          }}
+        />
+      </Header>
+      <Content
+        style={{
+          minHeight: "100vh",
+          color: "black",
+        }}
+      >
+        <UMBreadCrumb
+          items={[
+            {
+              label: `${base}`,
+              link: `/${base}`,
+            },
+            {
+              label: "student",
+              link: `/${base}/student`,
+            },
+          ]}
+        />
+        {children}
+      </Content>
+    </>
   );
 };
 
