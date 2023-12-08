f6074ef21a6a4fb5113d2e742e31c2ab080ef57d
f4faysal <f4faysals@gmail.com>
2023-12-09 00:32:59 +0600

    Refactor Antd theme configuration

--- src/app/(withlayout)/page.tsx
+++ src/app/(withlayout)/page.tsx
@@ -1,7 +1,32 @@
+import { Button, ConfigProvider } from "antd";
+
 export default function Home() {
   return (
     <main>
-      <h1>Local Areaa Tax Management</h1>
+      <ConfigProvider
+        theme={{
+          token: {
+            // Seed Token
+            colorPrimary: "#8dc5f8",
+            borderRadius: 2,
+
+            // Alias Token
+            colorBgContainer: "#b7dcfa",
+          },
+        }}
+      >
+        <h1>Local Areaa Tax Management</h1>
+        <Button
+          type="primary"
+          style={{
+            color: "black",
+          }}
+        >
+          Primary
+        </Button>
+        <br />
+        <Button>Default</Button>
+      </ConfigProvider>
     </main>
   );
 }
