2540ad0916bcddfab8cb4e3e8a5f87f950d55096
f4faysal <f4faysals@gmail.com>
2023-12-12 04:33:36 +0600

    Refactor code for improved performance and readability

---
+++ src/app/(withlayout)/account/admin/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>creat admin page</div>;
+};
+
+export default page;
---
+++ src/app/(withlayout)/account/permission/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>permission</div>;
+};
+
+export default page;
