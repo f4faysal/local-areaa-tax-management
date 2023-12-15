3ca7a8e74990122733a889542e52d8e363c3c760
f4faysal <f4faysals@gmail.com>
2023-12-15 22:45:49 +0600

    Fix bug in login functionality

---
+++ src/components/Forms/formSelectField.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Select } from "antd";
+import { Controller, useFormContext } from "react-hook-form";
+
+export type SelectOptions = {
+  label: string;
+  value: string;
+};
+
+type SelectFieldProps = {
+  options: SelectOptions[];
+  name: string;
+  size?: "large" | "small";
+  value?: string | string[] | undefined;
+  placeholder?: string;
+  label?: string;
+  defaultValue?: SelectOptions;
+  handleChange?: (el: string) => void;
+};
+
+const FormSelectField = ({
+  name,
+  size = "large",
+  value,
+  placeholder = "select",
+  options,
+  label,
+  defaultValue,
+  handleChange,
+}: SelectFieldProps) => {
+  const { control } = useFormContext();
+
+  return (
+    <>
+      {label ? label : null}
+      <Controller
+        control={control}
+        name={name}
+        render={({ field: { value, onChange } }) => (
+          <Select
+            onChange={handleChange ? handleChange : onChange}
+            size={size}
+            options={options}
+            value={value}
+            style={{ width: "100%" }}
+            placeholder={placeholder}
+            defaultValue={defaultValue}
+          />
+        )}
+      />
+    </>
+  );
+};
+
+export default FormSelectField;
