586cdab21ba8f33b92c1cbdad052b1cb1dcbdfaa
f4faysal <f4faysals@gmail.com>
2024-01-14 23:44:11 +0600

    Add logic for file upload

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  // Your function logic here
   return NextResponse.json({ name: "File uploaded" });
 }
